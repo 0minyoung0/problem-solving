c3e556735229d6232df665e2fd698f6124eefc5a
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 20:00:52 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 125 ms, Memory: 20,192 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,22 +1,28 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int case_num = 1; case_num <= T; case_num++) {
-			String s = br.readLine();
-			int ans = 0;
-			if (s.charAt(0) == '1') {
-				ans++;
-			}
-			for (int i=1; i<s.length(); i++) {
-				if (s.charAt(i) != s.charAt(i-1)) {
-					ans++;
-				}
-			}
-			System.out.println("#"+case_num+" "+ans);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 테스트 케이스의 수
+        int T = sc.nextInt();
+        
+        // 테스트 케이스 별로 실행
+        for (int t=1; t<=T; t++) {
+        	String s = sc.next();
+        	
+        	int ans = 0;
+        	if (s.charAt(0) == '1') {
+        		ans++;
+        	}
+        	for (int i=1; i<s.length(); i++) {
+        		if (s.charAt(i) != s.charAt(i-1)) {
+        			ans++;
+        		}
+        	}
+        	
+        	// 정답 출력
+        	System.out.println("#" + t + " " + ans);
+        }
+    }
 }
