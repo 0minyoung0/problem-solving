fa55a9e48a6f24cd7498e652ef2bf1d998219a95
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 15:41:40 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << a - b;
+
+	return 0;
+}
