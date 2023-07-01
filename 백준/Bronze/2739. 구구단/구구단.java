622d63e583c741c60e01c364316a35f38e99ac34
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 16:05:02 +0900

    [Bronze V] Title: 구구단, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i=1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+		
+	}
+}
