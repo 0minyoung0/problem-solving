7c1ff2277f2cefe32c5ceb45006c8ca23b2a01f3
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 20:03:24 +0900

    [D2] Title: 숫자 배열 회전, Time: 138 ms, Memory: 21,196 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,15 +1,44 @@
-T = int(input())
-for test_case in range(1, T + 1):
-	n = int(input())
-	arr = [list(map(int, input().split())) for _ in range (n)]
-	print(f"#{test_case}")
-	for i in range(n) :
-		for j in range(n) :
-			print(arr[n-1-j][i], end="")
-		print(" ", end="")
-		for j in range(n) :
-			print(arr[n-1-i][n-1-j], end="")
-		print(" ", end="")
-		for j in range(n) :
-			print(arr[j][n-1-i], end="")
-		print("")
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 테스트케이스 개수
+        int T = sc.nextInt();
+        
+        // 테스트케이스 별로 실행
+        for (int t=1; t<=T; t++) {
+        	// 배열의 크기
+        	int n = sc.nextInt();
+        	
+        	// 입력 배열
+        	int[][] arr = new int[n][n];
+        	for (int i=0; i<n; i++) {
+        		for (int j=0; j<n; j++) {
+        			arr[i][j] = sc.nextInt();
+        		}
+        	}
+        	// 출력 양식 맞춰서 바로 출력
+        	System.out.println("#" + t);
+        	for (int i=0; i<n; i++) {
+        		// 90도 돌린 배열
+        		for (int j=0; j<n; j++) {
+        			System.out.print(arr[n-j-1][i]);
+        		}
+        		System.out.print(" ");
+        		// 180도 돌린 배열
+        		for (int j=0; j<n; j++) {
+        			System.out.print(arr[n-i-1][n-j-1]);
+        		}
+        		System.out.print(" ");
+        		// 270도 돌린 배열
+        		for (int j=0; j<n; j++) {
+        			System.out.print(arr[j][n-i-1]);
+        		}
+        		System.out.println();
+        	}
+        	
+        }
+    }
+}
