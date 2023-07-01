6191582b6a78a60f2c86bf027f7d29d95cb0ed3b
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 19:26:59 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 134 ms, Memory: 50,496 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+def bfs (x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+    queue.append((x, y))
+    visited = [[0] * 16 for _ in range(16)]
+    visited[x][y] = 1
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= 16 or ny <0 or ny >= 16:
+                continue
+            if maze[nx][ny] == '3':
+                return 1
+            if maze[nx][ny] == '1' or visited[nx][ny] == 1:
+                continue
+            else:
+                queue.appendleft((nx, ny))
+                visited[nx][ny] = 1
+    return 0
+
+for test_case in range(1, 11):
+    case_num = int(input())
+    maze = []
+    for _ in range(16):
+        maze.append(input())
+    for i in range(16):
+        for j in range(16):
+            if maze[i][j] == '2':
+                start_x, start_y = i, j
+            elif maze[i][j] == '3':
+                end_x, end_y = i, j
+    print(f"#{case_num} {bfs(start_x, start_y)}")
