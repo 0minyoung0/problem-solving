52860d75f9f55a5f75390832642ba76e21c4c937
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 16:16:51 +0900

    [Silver IV] Title: 제로, Time: 196 ms, Memory: 21352 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int k = Integer.parseInt(br.readLine());
+		Deque<Integer> stack = new ArrayDeque<>();
+		
+		for (int i=0; i<k; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if (input == 0) {
+				stack.poll();
+			}
+			else {
+				stack.offerFirst(input);
+			}
+		}
+		
+		int sum = 0;
+		for (int num : stack) {
+			sum += num;
+		}
+		System.out.println(sum);
+	}
+}
