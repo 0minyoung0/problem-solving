4c80b7993a81592baaf4244b6a88170c9f1b6d4b
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 15:58:11 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, count;
+	cin >> n;
+	for (int i = 0;i < n;i++) {
+		count = i;
+		while (count) {
+			cout << ' ';
+			count--;
+		}
+		count = n - i;
+		while (count) {
+			cout << '*';
+			count--;
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
