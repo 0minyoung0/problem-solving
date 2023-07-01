31f8b5d5b4ac8a1a1606cbf5aa59f1455bc17d1d
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 15:56:10 +0900

    [Silver IV] Title: 로프, Time: 336 ms, Memory: 25248 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		List<Integer> ropes = new ArrayList<>();
+		for (int i=0; i<n; i++) {
+			ropes.add(Integer.parseInt(br.readLine()));
+		}
+		Collections.sort(ropes);
+		int ans = 0;
+		for (int i=0; i<n; i++) {
+			int temp = ropes.get(i) * (n - i);
+			if (temp > ans) {
+				ans = temp;
+			}
+		}
+		System.out.println(ans);
+	}
+}
