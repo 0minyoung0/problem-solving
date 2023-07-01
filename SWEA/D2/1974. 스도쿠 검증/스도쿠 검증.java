bef591437f67d7f67083725317c03aadab43647a
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 20:03:48 +0900

    [D2] Title: 스도쿠 검증, Time: 147 ms, Memory: 23,780 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -1,36 +1,77 @@
-T = int(input())
-for test_case in range(1, T + 1) :
-    isperfect = 1
-    arr = [list(map(int, input().split())) for _ in range (9)]
-    for i in range(9) :
-        temp_arr = sorted(arr[i])
-        if temp_arr != [1,2,3,4,5,6,7,8,9] :
-            isperfect = 0
-            break
-    if isperfect == 0 :
-        print(f"#{test_case} 0")
-        continue
-    for i in range(9) :
-        temp_arr = sorted([line[i] for line in arr])
-        if temp_arr != [1,2,3,4,5,6,7,8,9] :
-            isperfect = 0
-            break
-    if isperfect == 0 :
-        print(f"#{test_case} 0")
-        continue
-    for i in range(3) :
-        for j in range(3) :
-            temp_arr = []
-            for k in range(3) :
-                for l in range(3) :
-                    temp_arr.append(arr[3*i+k][3*j+l])
-            temp_arr.sort()
-            if temp_arr != [1,2,3,4,5,6,7,8,9] :
-                isperfect = 0
-                break
-        if isperfect == 0 :
-            break
-    if isperfect == 0:
-        print(f"#{test_case} 0")
-    else :
-        print(f"#{test_case} 1")
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 테스트케이스 개수
+        int T = sc.nextInt();
+        
+        // 테스트케이스 별로 실행
+        for (int t=1; t<=T; t++) {
+        	// 결과를 저장할 변수
+        	int ans = 1;
+        	
+        	// 배열에 스도쿠를 저장
+        	int[][] puz = new int[9][9];
+        	for (int i=0; i<9; i++) {
+        		for (int j=0; j<9; j++) {
+        			puz[i][j] = sc.nextInt();
+        		}
+        	}
+        	
+        	// 중복 숫자 체크를 위한 boolean 배열 선언
+        	boolean[] check;
+        	
+        	for (int i=0; i<9; i++) {
+        		// 가로 순회
+        		check = new boolean[10];
+        		for (int j=0; j<9; j++) {
+        			// 중복일 때
+        			if (check[puz[i][j]] == true) {
+        				ans = 0;
+        				break;
+        			}
+        			check[puz[i][j]] = true;
+        		}
+        		if (ans == 0) break;
+        		
+        		// 세로 순회
+        		check = new boolean[10];
+        		for (int j=0; j<9; j++) {
+        			// 중복일 때
+        			if (check[puz[j][i]] == true) {
+        				ans = 0;
+        				break;
+        			}
+        			check[puz[j][i]] = true;
+        		}
+        		if (ans == 0) break;
+        	}
+        	
+        	// 아직 중복 없으면 3x3도 검사
+        	if (ans == 1) {
+        		for (int i=0; i<3; i++) {
+        			for (int j=0; j<3; j++) {
+        				check = new boolean[10];
+        				for (int k=3*i; k<3*i+3; k++) {
+        					for (int l=3*j; l<3*j+3; l++) {
+        	        			// 중복일 때
+        	        			if (check[puz[k][l]] == true) {
+        	        				ans = 0;
+        	        				break;
+        	        			}
+        	        			check[puz[k][l]] = true;
+        					}
+        					if (ans == 0) break;
+        				}
+        				if (ans == 0) break;
+        			}
+        			if (ans == 0) break;
+        		}
+        	}
+        	
+        	System.out.println("#" + t + " " + ans);
+        }
+    }
+}
