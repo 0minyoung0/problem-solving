007cba522ffeeaaae19cea9aa8abdc1fc3d41854
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 15:55:56 +0900

    [Silver III] Title: 이친수, Time: 80 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[][] cnt = new long[n+1][2];
+		cnt[1][1] = 1;
+		for (int i=2; i<=n; i++) {
+			cnt[i][0] = cnt[i-1][0] + cnt[i-1][1];
+			cnt[i][1] = cnt[i-1][0];
+		}
+		System.out.println(cnt[n][0] + cnt[n][1]);
+	}
+}
