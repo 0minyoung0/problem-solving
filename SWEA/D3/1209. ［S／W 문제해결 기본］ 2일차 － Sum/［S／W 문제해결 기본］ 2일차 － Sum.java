6d957d1dd97171be46c4f66ef7fa9c3a832673cb
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-01 19:23:24 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 307 ms, Memory: 100,212 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,20 +1,52 @@
-for test_case in range(10):
-    test_num = int(input())
-    num_list = [list(map(int, input().split())) for _ in range(100)]
-    sum_max = 0
-    for i in range(100):
-        sum_max += num_list[i][i]
-    sum_cur = 0
-    for i in range(100):
-        sum_cur += num_list[i][99-i]
-    if sum_cur > sum_max:
-        sum_max = sum_cur
-    for i in range(100):
-        sum_cur = sum(num_list[i])
-        if sum_cur > sum_max:
-            sum_max = sum_cur
-    for i in range(100):
-        sum_cur = sum([line[i] for line in num_list])
-        if sum_cur > sum_max:
-            sum_max = sum_cur
-    print(f"#{test_num} {sum_max}")
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int TC=1; TC<=10; TC++) {
+			// 테스트케이스 숫자 필요 없어서 버림
+			sc.nextInt();
+			
+			// 배열 선언
+			int[][] arr = new int[100][100];
+			for (int i=0; i<100; i++) {
+				for (int j=0; j<100; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			// 최대값을 저장할 변수 ans, 인풋 범위가 애매해서 Integer 최소값으로 초기화
+			int ans = Integer.MIN_VALUE;
+			
+			// 가로행, 세로열 계산
+			for (int i=0; i<100; i++) {
+				int temp1 = 0;
+				int temp2 = 0;
+				for (int j=0; j<100; j++) {
+					// i행 합계 계산
+					temp1 += arr[i][j];
+					// i열 합계 계산
+					temp2 += arr[j][i];
+				}
+				// i행 합계와 i열 합계 중 큰 값이 ans 보다 크다면 ans에 할당
+				ans = Math.max(ans, Math.max(temp1, temp2));
+			}
+			
+			// 대각선 계산
+			int temp1 = 0;
+			int temp2 = 0;
+			for (int i=0; i<100; i++) {
+				// \ 방향 대각선 합계 계산
+				temp1 += arr[i][i];
+				// / 방향 대각선 합계 계산
+				temp2 += arr[i][99-i];
+			}
+			// 두 대각선 합계 중 큰 값이 ans 보다 크다면 ans에 할당
+			ans = Math.max(ans, Math.max(temp1, temp2));
+			
+			// 답 출력
+			System.out.println("#" + TC + " " + ans);
+		}
+	}
+}
