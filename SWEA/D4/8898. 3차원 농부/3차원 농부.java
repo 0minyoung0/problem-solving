64de7d98cf14ce2aaab0788ecf9c9091bcc57bee
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-07-03 12:09:25 +0900

    [D4] Title: 3차원 농부, Time: 5,067 ms, Memory: 297,044 KB -BaekjoonHub

---
+++ SWEA/D4/8898. 3차원 농부/3차원 농부.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		// 테스트 케이스의 수
+		int T = Integer.parseInt(br.readLine());
+		
+		// 테스트 케이스 별 실행
+		for (int tc=1; tc<=T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			
+			// 소의 수
+			int N = Integer.parseInt(st.nextToken());
+			
+			// 말의 수
+			int M = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			
+			// 소들의 x좌표
+			int c1 = Integer.parseInt(st.nextToken());
+			
+			// 말들의 x좌표
+			int c2 = Integer.parseInt(st.nextToken());
+			
+			// 소들의 위치
+			List<Integer> z1 = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+			for (int i=0; i<N; i++) {
+				z1.add(Integer.parseInt(st.nextToken()));
+			}
+			Collections.sort(z1);
+			
+			// 말들의 위치
+			List<Integer> z2 = new ArrayList<>();
+			st = new StringTokenizer(br.readLine());
+			for (int i=0; i<M; i++) {
+				z2.add(Integer.parseInt(st.nextToken()));
+			}
+			Collections.sort(z2);
+			
+			// 최소 거리를 저장할 변수
+			int minDis = Integer.MAX_VALUE;
+			
+			// 최소 거리를 만족하는 쌍의 개수를 저장할 변수
+			int cnt = 0;
+			
+			// 투포인터
+			int p1 = 0;
+			int p2 = 0;
+			while (p1 < N) {
+				int temp = Math.abs(z1.get(p1) - z2.get(p2));
+				if (minDis > temp) {
+					minDis = temp;
+					cnt = 1;
+				}
+				else if (minDis == temp) cnt++;
+				
+				if (p2 == M-1) p1++;
+				else if (p1 == N-1) p2++;
+				else if (z1.get(p1) <= z2.get(p2)) p1++;
+				else p2++;
+			}
+			
+			// 답 출력
+			System.out.println("#" + tc + " " + (minDis + Math.abs(c1 - c2)) + " " + cnt);
+		}
+	}
+}
---
+++ SWEA/D4/8898. 3차원 농부/README.md
@@ -0,0 +1,11 @@
+# [D4] 3차원 농부 - 8898 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AW45TzHae8UDFAQ7) 
+
+### 성능 요약
+
+메모리: 297,044 KB, 시간: 5,067 ms, 코드길이: 1,898 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
