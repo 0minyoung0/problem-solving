79a52198092bffc9db64eb1cd50ab01170ea122d
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2023-11-23 22:24:05 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10684 ms, Memory: 307396 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,77 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 컴퓨터의 개수
-		int N = Integer.parseInt(st.nextToken());
-		
-		// 신뢰하는 관계의 개수
-		int M = Integer.parseInt(st.nextToken());
-		
-		// 신뢰하는 관계를 인접리스트로 표현
-		List<Integer>[] graph = new List[N+1];
-		for (int i=1; i<=N; i++) {
-			graph[i] = new ArrayList<>();
-		}
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-			
-			// B를 해킹하면 A도 해킹 가능
-			graph[B].add(A);
-		}
-		
-		// 가장 많이 해킹한 컴퓨터의 수를 저장할 변수
-		int hackedN = 0;
-		
-		// 정답을 저장할 리스트
-		List<Integer> ans = new ArrayList<>();
-		
-		// 각 컴퓨터를 해킹했을 때를 BFS 시뮬
-		for (int i=1; i<=N; i++) {
-			
-			// BFS를 위한 Queue
-			Queue<Integer> q = new ArrayDeque<>();
-			q.offer(i);
-			
-			// 각 컴퓨터의 해킹 여부를 체크할 boolean 배열
-			boolean[] hacked = new boolean[N+1];
-			hacked[i] = true;
-			
-			// 해킹된 컴퓨터의 수를 셀 변수
-			int curHackedN = 1;
-			
-			// BFS 시뮬
-			while (!q.isEmpty()) {
-				int cur = q.poll();
-				for (int nx : graph[cur]) {
-					if (hacked[nx]) continue;
-					q.offer(nx);
-					hacked[nx] = true;
-					curHackedN++;
-				}
-			}
-			
-			// 정답 갱신 여부 체크
-			if (curHackedN < hackedN) continue;
-			if (curHackedN > hackedN) {
-				hackedN = curHackedN;
-				ans.clear();
-			}
-			ans.add(i);
-		}
-		
-		// 정답 출력
-		Collections.sort(ans);
-		StringBuilder sb = new StringBuilder();
-		for (int c : ans) {
-			sb.append(c).append(" ");
-		}
-		System.out.println(sb);
-	}
+
+    static int N, M;
+    static List<Integer>[] graph;
+    static Queue<Integer> q = new ArrayDeque<>();
+    static List<Integer> ansList = new ArrayList<>();
+    static int max;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAns();
+        printAns();
+    }
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        graph = new List[N+1];
+        for (int i=1; i<=N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[b].add(a);
+        }
+    }
+    private static void findAns() {
+        for (int cur=1; cur<=N; cur++) {
+            q.offer(cur);
+            boolean[] vis = new boolean[N+1];
+            vis[cur] = true;
+            int temp = 1;
+            while (!q.isEmpty()) {
+                int c = q.poll();
+                for (int nx : graph[c]) {
+                    if (vis[nx]) continue;
+                    q.offer(nx);
+                    vis[nx] = true;
+                    temp++;
+                }
+            }
+            if (max <= temp) {
+                if (max < temp) {
+                    ansList.clear();
+                    max = temp;
+                }
+                ansList.add(cur);
+            }
+        }
+    }
+    private static void printAns() {
+        StringBuilder sb = new StringBuilder();
+        for (int ans : ansList) sb.append(ans).append(" ");
+        System.out.print(sb);
+    }
 }
