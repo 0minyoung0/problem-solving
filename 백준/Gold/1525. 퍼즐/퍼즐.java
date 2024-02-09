676b239a480cfbbdb560e3d1a8d22c06ce3819d8
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2024-02-09 21:44:58 +0900

    [Gold II] Title: 퍼즐, Time: 472 ms, Memory: 47476 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -12,50 +12,68 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        String input = "";
+        int input = 0;
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                input += st.nextToken();
+                input = input * 10 + Integer.parseInt(st.nextToken());
             }
         }
 
-        if (input.equals("123456780")) {
+        if (input == 123456780) {
             System.out.println("0");
             return;
         }
 
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         map.put(input, 0);
 
-        Queue<String> q = new ArrayDeque<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.offer(input);
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
         while (!q.isEmpty()) {
-            String cur = q.poll();
-            int zeroPosX = cur.indexOf("0") / 3;
-            int zeroPosY = cur.indexOf("0") % 3;
+            int cur = q.poll();
+            int temp1 = cur;
+            int zeroPos = 0;
+            for (int i=0; i<9; i++) {
+                if (temp1 % 10 != 0) {
+                    temp1 /= 10;
+                }
+                else {
+                    zeroPos = i;
+                    break;
+                }
+            }
+            int zeroPosX = zeroPos / 3;
+            int zeroPosY = zeroPos % 3;
             for (int dir = 0; dir < 4; dir++) {
-                int nx = zeroPosX + dx[dir];
-                int ny = zeroPosY + dy[dir];
-                if (nx < 0 || nx >= 3 || ny < 0 || ny >= 3) continue;
-                char nextNum = cur.charAt(nx * 3 + ny);
-                String nextStr = cur.replace(nextNum, 'x')
-                                    .replace(cur.charAt(zeroPosX * 3 + zeroPosY), nextNum)
-                                    .replace('x', '0');
-                if (map.containsKey(nextStr)) continue;
-                if (nextStr.equals("123456780")) {
+                int nextPosX = zeroPosX + dx[dir];
+                int nextPosY = zeroPosY + dy[dir];
+                if (nextPosX < 0 || nextPosX >= 3 || nextPosY < 0 || nextPosY >= 3) continue;
+                int nextPos = nextPosX * 3 + nextPosY;
+                int nextNum = cur % (pow10(nextPosX * 3 + nextPosY) * 10) / pow10(nextPosX * 3 + nextPosY);
+
+                int next = cur + (pow10(zeroPos) - pow10(nextPos)) * nextNum;
+                if (map.containsKey(next)) continue;
+                if (next == 123456780) {
                     System.out.println(map.get(cur) + 1);
                     return;
                 }
-                map.put(nextStr, map.get(cur) + 1);
-                q.offer(nextStr);
+                map.put(next, map.get(cur) + 1);
+                q.offer(next);
             }
         }
 
         System.out.println("-1");
+    }
+    private static int pow10 (int n) {
+        int result = 1;
+        while (n-- > 0) {
+            result *= 10;
+        }
+        return result;
     }
 }
