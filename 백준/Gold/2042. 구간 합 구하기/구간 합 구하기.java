b8f4a087db66a331c9e4fb01ccf70cfd323a808f
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2025-02-25 18:59:09 +0900

    [Gold I] Title: 구간 합 구하기, Time: 512 ms, Memory: 93440 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,81 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N;
+    static long[] fenwickTree;
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        // 수의 개수 N
-        int N = Integer.parseInt(st.nextToken());
-
-        // 수의 변경이 일어나는 횟수 M
+        N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        // 구간의 합을 구하는 횟수 K
         int K = Integer.parseInt(st.nextToken());
-
-        // 배열에 N개의 수 저장
-        long[] a = new long[N];
-        for (int i=0; i<N; i++) {
-            a[i] = Long.parseLong(br.readLine());
+        
+        long[] arr = new long[N + 1];
+        fenwickTree = new long[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+            update(i, arr[i]);
         }
 
-        // 세그먼트 트리 만들기
-        int h = (int)Math.ceil(Math.log(N) / Math.log(2));
-        int tree_size = 1 << (h + 1);
-        long[] tree = new long[tree_size];
-        init(a, tree, 1, 0, N - 1);
-
-        // M + K 개의 쿼리 처리
-        for (int i=0; i<M+K; i++) {
+        StringBuilder sb = new StringBuilder();
+        while (M > 0 || K > 0) {
             st = new StringTokenizer(br.readLine());
-
-            // 수 변경
             if (st.nextToken().equals("1")) {
-                int index = Integer.parseInt(st.nextToken()) - 1;
-                long val = Long.parseLong(st.nextToken());
-                update(a, tree, 1, 0, N-1, index, val);
-            }
-
-            // 구간 합 출력
-            else {
-                int left = Integer.parseInt(st.nextToken()) - 1;
-                int right = Integer.parseInt(st.nextToken()) - 1;
-                sb.append(query(tree, 1, 0, N - 1, left, right)).append("\n");
+                int b = Integer.parseInt(st.nextToken());
+                long c = Long.parseLong(st.nextToken());
+                update(b, c - arr[b]);
+                arr[b] = c;
+                M--;
+            } else {
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+                sb.append(sum(c) - sum(b - 1)).append('\n');
+                K--;
             }
         }
-
-        // 답 출력
         System.out.print(sb);
     }
-    public static void init(long[] a, long[] tree, int node, int start, int end) {
-        if (start == end) tree[node] = a[start];
-        else {
-            init(a, tree, node * 2, start, (start + end) / 2);
-            init(a, tree, node * 2 + 1, (start + end) / 2 + 1, end);
-            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+
+    private static long sum(int pos) {
+        long ret = 0;
+        while (pos > 0) {
+            ret += fenwickTree[pos];
+            pos &= (pos - 1);
         }
+        return ret;
     }
-    public static long query(long[] tree, int node, int start, int end, int left, int right) {
-        if (end < left || right < start) return 0;
-        if (left <= start && end <= right) return tree[node];
-        long lsum = query(tree, node * 2, start, (start + end) / 2, left, right);
-        long rsum = query(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
-        return lsum + rsum;
-    }
-    public static void update(long[] a, long[] tree, int node, int start, int end, int index, long val) {
-        if (index < start || end < index) return;
-        if (start == end) {
-            a[index] = val;
-            tree[node] = val;
-            return;
+
+    private static void update(int pos, long val) {
+        while (pos <= N) {
+            fenwickTree[pos] += val;
+            pos += (pos & -pos);
         }
-        update(a, tree, node * 2, start, (start + end) / 2, index, val);
-        update(a, tree, node * 2 + 1, (start + end) / 2 + 1, end, index, val);
-        tree[node] = tree[node * 2] + tree[node * 2 + 1];
     }
 }
