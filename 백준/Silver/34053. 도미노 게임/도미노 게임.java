11e786992af18690b49bc09a49ab577124013aa6
0minyoung0 <122426037+0minyoung0@users.noreply.github.com>
2025-07-20 15:23:21 +0900

    [Silver III] Title: 도미노 게임, Time: 512 ms, Memory: 108472 KB -BaekjoonHub

---
+++ 백준/Silver/34053. 도미노 게임/도미노 게임.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long ans = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                ans += num;
+                if (min > num) {
+                    min = num;
+                }
+            }
+        }
+        ans -= min;
+
+        System.out.println(ans);
+    }
+}
